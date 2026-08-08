416630ff49246b547125f85666242d153fa38174
omidafifi <afifiomid@yahoo.com>
2026-08-08 17:08:54 +0330

    my second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class  Main {
     static void main(String[] args) {
-        System.out.println("test");
-
+        System.out.println("test1");
+        System.out.println("test2");
     }
 }
