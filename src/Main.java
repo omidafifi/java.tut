42505fb3ef50d1fca82c56640f3fe50f190b4add
omidafifi <afifiomid@yahoo.com>
2026-08-08 22:57:11 +0330

    my third commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class  Main {
     static void main(String[] args) {
-        System.out.println("test1");
-        System.out.println("test2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Application started");
+        System.out.println("............");
+        System.out.println("Please enter your name : ");
+        String userInput = scanner.nextLine();
+        System.out.println("Hallo" +" " + userInput);
+        scanner.close();
+
     }
 }
